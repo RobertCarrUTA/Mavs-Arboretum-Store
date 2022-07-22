fc1c374fc331572397e5efe3ee536693babb47dc
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-21 21:04:10 -0500

    Changing formatting

--- tool.h
+++ tool.h
@@ -3,11 +3,11 @@
 
 #include "product.h"
 
-class Tool : public Product {
-  public:
-    Tool(std::string name, double price, std::string description);
-    Tool(std::istream& ist);       // check lecture 17 canvas.h and canvas.cpp
-    void save(std::ostream& ost) override;
+class Tool: public Product
+{
+  public: Tool(std::string name, double price, std::string description);
+  Tool(std::istream & ist); // check lecture 17 canvas.h and canvas.cpp
+  void save(std::ostream & ost) override;
 };
 
 #endif

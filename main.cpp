56a6708ae283a3fce96923900d148e683a0be183
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-21 22:27:00 -0500

    Changing formatting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "mainwin.h"
 
-int main (int argc, char *argv[]) {
-    auto app = Gtk::Application::create(argc, argv, "edu.uta.cse1325.app.manga");
-    Mainwin win;
-    return app->run(win);
+int main(int argc, char * argv[])
+{
+  auto app = Gtk::Application::create(argc, argv, "edu.uta.cse1325.app.manga");
+  Mainwin win;
+  return app -> run(win);
 }

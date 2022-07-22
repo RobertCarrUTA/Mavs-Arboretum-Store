b26760da7a48fe4c449b36326f348b471aefd564
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-21 21:25:05 -0500

    Changing formatting

--- order.h
+++ order.h
@@ -5,17 +5,18 @@
 #include "customer.h"
 #include <vector>
 
-class Order {
-    public:
-        Order(Customer customer);
-        Order(std::istream& ist);
-        void save(std::ostream& ost);
-        void add_item(Item item);
-        double total() const;
-        friend std::ostream& operator<<(std::ostream& ost, const Order& order);
+class Order
+{
+  public:
+    Order(Customer customer);
+    Order(std::istream & ist);
+    void save(std::ostream & ost);
+    void add_item(Item item);
+    double total() const;
+    friend std::ostream & operator << (std::ostream & ost, const Order & order);
   private:
-        Customer _customer;
-        std::vector<Item> _items;
+    Customer _customer;
+    std::vector < Item > _items;
 };
 
 #endif

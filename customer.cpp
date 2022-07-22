f0dad91ad079e249d65f5517f99121cb68f66eef
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-21 22:36:35 -0500

    Changing formatting

--- customer.cpp
+++ customer.cpp
@@ -3,23 +3,27 @@
 Customer::Customer(std::string name, std::string phone, std::string email)
     : _name{name}, _phone{phone}, _email{email} {}
 
-void Customer::save(std::ostream& ost) {
+void Customer::save(std::ostream& ost)
+{
     ost << _name << '\n';
     ost << _phone << '\n';
     ost << _email << '\n';
 }
 
-Customer::Customer(std::istream& ist) {
+Customer::Customer(std::istream& ist)
+{
     std::getline(ist, _name);
     std::getline(ist, _phone);
     std::getline(ist, _email);
 }
 
-std::string Customer::to_string() const {
+std::string Customer::to_string() const
+{
     return _name + " (" + _phone + ", " + _email + ")";
 }
 
-std::ostream& operator<<(std::ostream& ost, const Customer& customer) {
+std::ostream& operator<<(std::ostream& ost, const Customer& customer)
+{
     ost << customer.to_string();
     return ost;
 }

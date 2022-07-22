16b00b0d194704666dc920320b48375f6efb9967
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-07-21 22:41:50 -0500

    Changing formatting

--- cli.cpp
+++ cli.cpp
@@ -3,92 +3,91 @@
 #include <vector>
 #include <gtkmm.h>
 
-std::string get_string(std::string prompt) {
-    std::string s;
-    std::cout << prompt;
-    std::getline(std::cin, s);
-    return s;
+std::string get_string(std::string prompt)
+{
+  std::string s;
+  std::cout << prompt;
+  std::getline(std::cin, s);
+  return s;
 }
 
 double get_double(std::string prompt)
 {
-    while(true)
+  while(true)
+  {
+    try
     {
-        try
-        {
-            return std::stod(get_string(prompt));
-        }
-        catch(std::exception& e)
-        {
-            std::cerr << "ERROR: " << e.what() << std::endl;
-        }
+      return std::stod(get_string(prompt));
     }
+    catch(std::exception& e)
+    {
+      std::cerr << "ERROR: " << e.what() << std::endl;
+    }
+  }
 }
 
 int get_int(std::string prompt)
 {
-    while(true)
+  while(true)
+  {
+    try
     {
-        try
-        {
-            return std::stoi(get_string(prompt));
-        }
-        catch(std::exception& e)
-        {
-            std::cerr << "ERROR: " << e.what() << std::endl;
-        }
+      return std::stoi(get_string(prompt));
     }
+    catch(std::exception& e)
+    {
+      std::cerr << "ERROR: " << e.what() << std::endl;
+    }
+  }
 }
 
 Exposure get_exposure()
 {
-    while(true)
+  while(true)
+  {
+    switch (get_int("Select Exposure (1) Shade (2) Part Sun (3) Sun? "))
     {
-        switch (get_int("Select Exposure (1) Shade (2) Part Sun (3) Sun? "))
-        {
-            case 1: return Exposure::SHADE;
-            case 2: return Exposure::PARTSUN;
-            case 3: return Exposure::SUN;
-            default: std::cerr << "INVALID SELECTION\n" << std::endl;
-        }
+      case 1: return Exposure::SHADE;
+      case 2: return Exposure::PARTSUN;
+      case 3: return Exposure::SUN;
+      default: std::cerr << "INVALID SELECTION\n" << std::endl;
     }
+  }
 }        
         
 Material get_material()
 {
-    while(true)
+  while(true)
+  {
+    switch (get_int("Select Material (1) Rubber (2) Pine (3) Cedar (4) Hardwood? "))
     {
-        switch (get_int("Select Material (1) Rubber (2) Pine (3) Cedar (4) Hardwood? "))
-        {
-            case 1: return Material::RUBBER;
-            case 2: return Material::PINE;
-            case 3: return Material::CEDAR;
-            case 4: return Material::HARDWOOD;
-            default: std::cerr << "INVALID SELECTION\n" << std::endl;
-        }
+      case 1: return Material::RUBBER;
+      case 2: return Material::PINE;
+      case 3: return Material::CEDAR;
+      case 4: return Material::HARDWOOD;
+      default: std::cerr << "INVALID SELECTION\n" << std::endl;
     }
+  }
 }        
 
 int main()
 {
-    std::vector<Product*> products;
-    
-   
-    
-    while(true)
+  std::vector<Product*> products;
+
+  while(true)
+  {
+    switch (get_int("Create new (1) Tool (2) Plant (3) Mulch or (0) Exit? "))
     {
-        switch (get_int("Create new (1) Tool (2) Plant (3) Mulch or (0) Exit? "))
-        {
-            case 0: exit(0);
-            case 1: products.push_back(new Tool(get_string("Name? "), get_double("Price? "), get_string("Description? "))); break;
-            case 2: products.push_back(new Plant(get_string("Name? "), get_double("Price? "), get_string("Description? "),
-                        get_string("Species? "), get_exposure())); break;
-            case 3: products.push_back(new Mulch(get_string("Name? "), get_double("Price? "), get_string("Description? "),
-                        get_int("Volume (ft³)? "), get_material())); break;EntryDialog one{*this, "New Mulch\nDescription?"};
+      case 0: exit(0);
+      case 1: products.push_back(new Tool(get_string("Name? "), get_double("Price? "), get_string("Description? "))); break;
+      case 2: products.push_back(new Plant(get_string("Name? "), get_double("Price? "), get_string("Description? "),
+        get_string("Species? "), get_exposure())); break;
+      case 3: products.push_back(new Mulch(get_string("Name? "), get_double("Price? "), get_string("Description? "),
+        get_int("Volume (ft³)? "), get_material())); break; EntryDialog one{ *this, "New Mulch\nDescription?" };
         one.run();
-            default: std::cerr << "ERROR: Invalid option\n";
-        }
-        std::cout << "\nCurrent Catalog\n===============\n";
-        for(auto product : products) std::cout << *product << "\n\n";
+      default: std::cerr << "ERROR: Invalid option\n";
     }
+    std::cout << "\nCurrent Catalog\n===============\n";
+    for(auto product : products) std::cout << *product << "\n\n";
+  }
 }
